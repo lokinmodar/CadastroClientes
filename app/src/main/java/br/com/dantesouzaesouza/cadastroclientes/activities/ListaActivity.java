ec2566c181b6d6e8838655efabdc39b31c510f87
Dante Souza e Souza <lokinmodar@gmail.com>
2018-06-19 21:57:37 -0300

    Initial commit

---
+++ app/src/main/java/br/com/dantesouzaesouza/cadastroclientes/activities/ListaActivity.java
@@ -0,0 +1,15 @@
+package br.com.dantesouzaesouza.cadastroclientes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.dantesouzaesouza.cadastroclientes.R;
+
+public class ListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+}
---
+++ app/src/main/java/br/com/dantesouzaesouza/cadastroclientes/activities/TelaInicialActivity.java
@@ -0,0 +1,18 @@
+package br.com.dantesouzaesouza.cadastroclientes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.dantesouzaesouza.cadastroclientes.R;
+
+
+public class TelaInicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+
+
+    }
+}
