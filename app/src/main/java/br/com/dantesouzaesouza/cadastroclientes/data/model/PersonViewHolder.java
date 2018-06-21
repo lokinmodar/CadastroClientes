fdb5f9565a4a769d306c97c7ef977b0f5a156712
Dante Souza e Souza <lokinmodar@gmail.com>
2018-06-21 18:49:59 -0300

    Criação de Activities
    Cadastro
    Listagem
    Tela Inicial
    
    Implementando layouts e classes de apoio

--- app/src/main/java/br/com/dantesouzaesouza/cadastroclientes/data/model/PersonViewHolder.java
+++ app/src/main/java/br/com/dantesouzaesouza/cadastroclientes/data/model/PersonViewHolder.java
@@ -1,15 +1,34 @@
 package br.com.dantesouzaesouza.cadastroclientes.data.model;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
-public class PersonViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+import br.com.dantesouzaesouza.cadastroclientes.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PersonViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+
+    @BindView(R.id.itemListaCardView) CardView clienteCard;
+    @BindView(R.id.nomeTextView) TextView nomeTextView;
+    @BindView(R.id.cpfTextView) TextView cpfTextView;
+    @BindView(R.id.idadeTextView) TextView idadeTextView;
+    @BindView(R.id.telefoneTextView) TextView telefoneTextView;
+    @BindView(R.id.cidadeTextView) TextView cidadeTextView;
+    @BindView(R.id.codigoTextView) TextView codigoTextView;
+
     public PersonViewHolder(View itemView) {
         super(itemView);
+        itemView.setOnLongClickListener(this);
+        ButterKnife.bind(this, itemView);
+
+
     }
 
     @Override
-    public void onClick(View v) {
-
+    public boolean onLongClick(View v) {
+        return false;
     }
 }
